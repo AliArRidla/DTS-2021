83ccd627745e1cfcff10a4fb8878dd4376dcd88f
zero0dayali <beeunik0@gmail.com>
2021-07-30 07:32:48 +0700

    latihan 2 pertemuan 2

--- app/src/main/java/com/polinema/dts2021_pertemuan2/MainActivity.java
+++ app/src/main/java/com/polinema/dts2021_pertemuan2/MainActivity.java
@@ -6,9 +6,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextClock;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+//    deklarasi
+    EditText editTextNilai1;
+    EditText editTextNilai2;
+    TextView textViewHasil;
+    Button buttonHitung;
+    int nilai1,nilai2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,29 +24,49 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 //        inisialisasi
-        EditText editTextJari = findViewById(R.id.jari);
-        TextView textViewKeliling = findViewById(R.id.textViewKeliling);
-        TextView textViewLuas = findViewById(R.id.textViewLuas);
-        Button buttonHitung = findViewById(R.id.buttonHitung);
+        editTextNilai1 = findViewById(R.id.editTextNilai1);
+        editTextNilai2 = findViewById(R.id.editTextNilai2);
+        textViewHasil = findViewById(R.id.textViewHasil);
+        buttonHitung = findViewById(R.id.buttonHitung);
+
+//        EditText editTextJari = findViewById(R.id.editTextNilai1);
+//        TextView textViewKeliling = findViewById(R.id.textViewKeliling);
+//        TextView textViewLuas = findViewById(R.id.textViewLuas);
+//        Button buttonHitung = findViewById(R.id.buttonHitung);
 
 //        aksi
-        buttonHitung.setOnClickListener(new View.OnClickListener(){
 
+        buttonHitung.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
+            public void onClick(View v) {
+//                bandingkan
+                if (nilai1 > nilai2){
+                    System.out.println("lebih besar nilai 1");
+                    textViewHasil.setText("Lebih besar nilai 1");
+                }else {
+                    System.out.println("lebih besar nilai 2");
+                    textViewHasil.setText("Lebih besar nilai 2");
+                }
+            }
+        });
+
+//        buttonHitung.setOnClickListener(new View.OnClickListener(){
+
+//            @Override
+//            public void onClick(View view){
 //                convert
-                float inoutJariJari = Float.parseFloat(editTextJari.getText().toString());
+//                float inoutJariJari = Float.parseFloat(editTextJari.getText().toString());
 
 //                inisiasi & deklarasi rumus
-                double keliling = inoutJariJari*2* Math.PI;
-                double luas = Math.pow(inoutJariJari,2)* Math.PI;
+//                double keliling = inoutJariJari*2* Math.PI;
+//                double luas = Math.pow(inoutJariJari,2)* Math.PI;
 
 //                set nilai
-                textViewKeliling.setText(String.valueOf(keliling));
-                textViewLuas.setText(String.valueOf(luas));
-            }
+//                textViewKeliling.setText(String.valueOf(keliling));
+//                textViewLuas.setText(String.valueOf(luas));
+//            }
 
-        });
+//        });
 
     }
 }
